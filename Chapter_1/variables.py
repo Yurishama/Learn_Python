9155b454b01182e32ce4f3688e6f3c943db395c4
Saida Reyes <yurishama@gmail.com>
2025-07-15 00:03:13 +0200

    add Chapter1

---
+++ Chapter_1/variables.py
@@ -0,0 +1,15 @@
+#Variables in python are case sensitive 
+_cars= 1
+cars = 24
+CARS = 2
+number_of_cars = 23
+kind_of_cars = "BMW"
+# here different variables
+
+
+
+print(_cars)
+print(cars)
+print(CARS)
+print(number_of_cars)
+print(kind_of_cars)
